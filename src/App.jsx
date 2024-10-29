a6cd05cc38e86d3a4b8ab8fa9bc04438c89a5e76
im-ravira <imravira@gmail.com>
2024-10-29 09:59:24 +0530

    chore: initialize project with Vite, React, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-blue-500 text-8xl text-center mt-48'>Initial Setup</h1>  
+    </>
+  )
+}
+
+export default App
