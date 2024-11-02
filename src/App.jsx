8496296dbdaf14f00295355012135903de37942d
im-ravira <imravira@gmail.com>
2024-11-02 21:46:50 +0530

    feat: Update App component for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Hero from "./Hero";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HeroPage from "./pages/HeroPage";
+import NewsPage from "./pages/NewsPage";
 
 function App() {
-
   return (
-    <Hero />
-  )
+    <Router>
+      <Routes>
+        {/* Hero page as landing page */}
+        <Route path="/" element={<HeroPage />} />
+        {/* News page route with dynamic category */}
+        <Route path="/news/:category" element={<NewsPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
